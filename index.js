7cbda4395afc4042bb6f181fe7e9dbcef8942abc
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-05-03 04:34:54 +0600

    vercel json

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-const catagories = require('./data/catagories.json')
+const catagories = require('./data/catagories.json');
+
+const news = require('./data/news.json');
 
 app.use(cors())
 
@@ -14,6 +16,42 @@ app.get('/', (req, res) => {
 app.get("/catagories", (req, res) => {
     res.send(catagories);
 })
+
+
+app.get("/news", (req, res) => {
+    res.send(news)
+})
+
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedId = news.find(pd => pd._id == id)
+    res.send(selectedId);
+})
+
+app.get('/catagories/:id', (req, res) => {
+    // const id = req.params.id
+    const id = parseInt(req.params.id)
+
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+
+        const selectedId = news.filter(pd => parseInt(pd.category_id) == id)
+        res.send(selectedId)
+    }
+    // if (id == 0) {
+    //     res.send(news)
+    // }
+    // else {
+
+    //     const selectedId = news.filter(pd => pd.category_id == id)
+    //     res.send(selectedId)
+    // }
+
+})
+
 
 app.listen(port, () => {
     console.log(`Tiger API is running on port: ${port}`);
